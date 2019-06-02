64ffefdd45eb2883a5aeaeaf966c6131d6b0b550
Argonautic <jonathan.zhou93@gmail.com>
2019-06-02 08:44:50 -0400

    Added a few basic expression parsing functions, created ParseRule and Precedence structs for handling function getting and precedence, and created functions to handle precedence

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -12,6 +12,29 @@ typedef struct {
     bool panicMode;
 } Parser;
 
+/** Precedence levels sorted lowest to highest */
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,  // =
+    PREC_OR,          // or
+    PREC_AND,         // and
+    PREC_EQUALITY,    // == !=
+    PREC_COMPARISON,  // < > <= >=
+    PREC_TERM,        // + -
+    PREC_FACTOR,      // * /
+    PREC_UNARY,       // ! -
+    PREC_CALL,        // . () []
+    PREC_PRIMARY
+} Precedence;
+
+typedef void (*ParseFn)();  // ParseFn is a func that takes no arguments and returns nothing
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
 Parser parser;  // Single global variable like vm and scanner. Make factory if using for prod
 
 Chunk* compilingChunk;
@@ -81,8 +104,8 @@ static void emitByte(uint8_t byte) {
     Emit two bytes consecutively. Convenience function for emitting opcode byte + operand byte
  */
 static void emitBytes(uint8_t byte1, uint8_t byte2) {
-    emitByte(1);
-    emitByte(2);
+    emitByte(byte1);
+    emitByte(byte2);
 }
 
 /**
@@ -113,6 +136,34 @@ static void endCompiler() {
     emitReturn();
 }
 
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void binary() {
+    // Remember the operator.
+    TokenType operatorType = parser.previous.type;
+
+    // Compile the right operand
+    ParseRule* rule = getRule(operatorType);
+    parsePrecedence((Precedence)(rule->precedence + 1));
+
+    // Emit the operator instruction.
+    switch (operatorType) {
+        case TOKEN_PLUS:    emitByte(OP_ADD); break;
+        case TOKEN_MINUS:   emitByte(OP_SUBTRACT); break;
+        case TOKEN_STAR:    emitByte(OP_MULTIPLY); break;
+        case TOKEN_SLASH:   emitByte(OP_DIVIDE); break;
+        default:
+            return; // Unreachable
+    }
+}
+
+static void grouping() {
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
 /**
     Emit one number constant
  */
@@ -121,8 +172,103 @@ static void number() {
     emitConstant(value);
 }
 
-void expression() {
+static void unary() {
+    TokenType operatorType = parser.previous.type;
 
+    // Compile the operand
+    parsePrecedence(PREC_UNARY);
+
+    // Emit the operator instructions
+    switch (operatorType) {
+        case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+        default:
+            return;  // Unreachable
+    }
+}
+
+/**
+    Rules table for creating expressions. Prefix prop handles which tokens begin a new expression starting from that
+    token, infix prop handles which tokens indiciate an expression using previous and succeeding tokens, and precedence
+    prop indicates precedence
+
+    TODO: Not all rules are filled in yet. Until they are, placeholder is { NULL, NULL, PREC_NONE }
+ */
+ParseRule rules[] = {
+    { grouping, NULL,    PREC_CALL },       // TOKEN_LEFT_PAREN
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_RIGHT_PAREN
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_LEFT_BRACE
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_RIGHT_BRACE
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_COMMA
+    { NULL,     NULL,    PREC_CALL },       // TOKEN_DOT
+    { unary,    binary,  PREC_TERM },       // TOKEN_MINUS
+    { NULL,     binary,  PREC_TERM },       // TOKEN_PLUS
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_SEMICOLON
+    { NULL,     binary,  PREC_FACTOR },     // TOKEN_SLASH
+    { NULL,     binary,  PREC_FACTOR },     // TOKEN_STAR
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_BANG
+    { NULL,     NULL,    PREC_EQUALITY },   // TOKEN_BANG_EQUAL
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_EQUAL
+    { NULL,     NULL,    PREC_EQUALITY },   // TOKEN_EQUAL_EQUAL
+    { NULL,     NULL,    PREC_COMPARISON }, // TOKEN_GREATER
+    { NULL,     NULL,    PREC_COMPARISON }, // TOKEN_GREATER_EQUAL
+    { NULL,     NULL,    PREC_COMPARISON }, // TOKEN_LESS
+    { NULL,     NULL,    PREC_COMPARISON }, // TOKEN_LESS_EQUAL
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_STRING
+    { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER
+    { NULL,     NULL,    PREC_AND },        // TOKEN_AND
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_ELSE
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_FALSE
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_FOR
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_FUN
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_IF
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_NIL
+    { NULL,     NULL,    PREC_OR },         // TOKEN_OR
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_PRINT
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_RETURN
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_SUPER
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_THIS
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_TRUE
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_VAR
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_WHILE
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_ERROR
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_EOF
+};
+
+/**
+    Get an expression based on precedence, with *precendence* being the max precedence the expression may have (higher
+    precedence means LOWER enum value in the Precedence struct)
+ */
+static void parsePrecedence(Precedence precedence) {
+    advance();
+    ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+
+    // The first token in an expression will ALWAYS be a prefix expression, whether a literal (like a number) or a unary
+    if (prefixRule == NULL) {
+        error("Expect expression.");
+        return;
+    }
+    prefixRule();
+
+    // Only continue parsing for infix expressions if the infix expression has lower precedence than *precedence*. If
+    // next token has too high precedence, or isn't an infix operator, expression is done
+    while (precedence <= getRule(parser.current.type)->precedence) {
+        advance();
+        ParseFn infixRule = getRule(parser.previous.type)->infix;
+        infixRule();
+    }
+}
+
+/**
+    Get the ParseRule at the index *type*
+ */
+static ParseRule* getRule(TokenType type) {
+    return &rules[type];
+}
+
+void expression() {
+    parsePrecedence(PREC_ASSIGNMENT);
 }
 
 bool compile(const char* source, Chunk* chunk) {
