884ee538214f9964c21c0b9b738a432ff55d93aa
Argonautic <jonathan.zhou93@gmail.com>
2019-06-03 08:06:16 -0400

    Defined macros to process and instantie new Values from C primitives (code still broken because many functions expect Value to be a double)

--- src/value/value.h
+++ src/value/value.h
@@ -28,6 +28,20 @@ typedef struct {
     } as;
 } Value;
 
+// Macros to check the ValueType of a Value
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value),type == VAL_NIL)
+#define IS_NUMBER         ((value).type == VAL_NUMBER)
+
+// Macros to get C primitives out of Value *as* fields. Should always be guarded with the corresponding IS_TYPE macro
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+
+// Macros to instantiate new Values from C primitives
+#define BOOL_VAL(value)   ((Value){ VAL_BOOL, { .boolean = value } })
+#define NIL_VAL           ((Value){ VAL_NIL, { .number = 0 } })
+#define NUMBER_VAL(value) ((Value){ VAL_NUMBER, { .number = value } })
+
 typedef struct {
     int capacity;
     int count;
