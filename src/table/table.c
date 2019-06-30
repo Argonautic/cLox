31f4da6b43a751e072f0440507d6fad671b99810
Argonautic <jonathan.zhou93@gmail.com>
2019-06-30 14:08:26 -0400

    Hash table now can get/set and adjust capacity dynamically. Added function to duplicate tables

--- src/table/table.c
+++ src/table/table.c
@@ -6,6 +6,8 @@
 #include "table.h"
 #include "../value/value.h"
 
+#define TABLE_MAX_LOAD 0.75
+
 void initTable(Table* table) {
     table->count = 0;
     table->capacity = 0;
@@ -15,4 +17,81 @@ void initTable(Table* table) {
 void freeTable(Table* table) {
     FREE_ARRAY(Entry, table->entries, table->capacity);
     initTable(table);
+}
+
+/**
+    Find an entry in your hash table by key. Hash table uses linear probing to find an entry in destination bucket or
+    forward adjacent buckets. Uses a pointer to Entries array rather than pointer to Table because some operations have
+    to be done on Entries arrays that aren't part of Tables yet
+ */
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+    uint32_t index = key->hash % capacity;
+
+    // Probe until you find the entry or a bucket that can contain the new entry. Guaranteed to find at least one empty
+    // bucket because max load factor is 75%
+    for (;;) {
+        Entry* entry = &(entries[index]);
+
+        if (entry->key == key || entry->key == NULL) {
+            return entry;
+        }
+
+        index = (index + 1) % capacity;
+    }
+}
+
+/**
+    Adjust the capacity of a hash Table and sort existing Entries into new buckets based on new capacity
+ */
+static void adjustCapacity(Table* table, int capacity) {
+    Entry* entries = ALLOCATE(Entry, capacity);
+    for (int i = 0; i < capacity; i++) {
+        entries[i].key = NULL;
+        entries[i].value = NIL_VAL;
+    }
+
+    // Copy over any existing entries in old entries array. Remap them to the appropriate bucket in new array
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &(table->entries[i]);
+        if (entry->key == NULL) continue;
+
+        Entry* dest = findEntry(entries, capacity, entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+    }
+
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    table->entries = entries;
+    table->capacity = capacity;
+}
+
+/**
+    Set an entry in a table
+ */
+bool tableSet(Table* table, ObjString* key, Value value) {
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustCapacity(table, capacity);
+    }
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+
+    bool isNewKey = entry->key == NULL;
+    if (isNewKey) table->count++;
+
+    entry->key = key;
+    entry->value = value;
+    return isNewKey;
+}
+
+/**
+    Copy over the contents of one table to another
+ */
+void tableAddAll(Table* from, Table* to) {
+    for (int i = 0; i < from->capacity; i++) {
+        Entry* entry = &(from->entries[i]);
+        if (entry->key != NULL) {
+            tableSet(to, entry->key, entry->value);
+        }
+    }
 }
--- src/table/table.h
+++ src/table/table.h
@@ -17,5 +17,7 @@ typedef struct {
 
 void initTable(Table* table);
 void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
+void tableAddAll(Table* from, Table* to);
 
 #endif
